ea21b3e8dfb52d66762c5f9feb4e6218bf945569
Sai Kiran Singh <116514088+saikiran070@users.noreply.github.com>
2024-03-21 00:54:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// JavaScript code for the portfolio website
+
+// Function to display an alert when the contact button is clicked
+function showAlert() {
+    alert("Contact form functionality is not implemented in this demo.");
+  }
+  
